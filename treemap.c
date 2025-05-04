2158ceec820251e3a61cfd3faa64d423881d5f28
Your Name <you@example.com>
2025-05-04 00:10:13 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -199,5 +199,26 @@ Pair * firstTreeMap(TreeMap * tree) {
 }
 
 Pair * nextTreeMap(TreeMap * tree) {
-    return NULL;
+    TreeNode* current = tree->current;
+    if (current->right != NULL) {
+        current = current->right;
+        while (current->left != NULL) {
+            current = current->left;
+        }
+        tree->current = current;
+        return current->pair;
+    }
+    TreeNode* parent = current->parent;
+    while (parent != NULL && current == parent->right) {
+        current = parent;
+        parent = parent->parent;
+    }
+    
+    tree->current = parent;
+    if (parent != NULL) {
+        return parent->pair;
+    } 
+    else {
+        return NULL;
+    }
 }
